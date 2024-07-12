52c9b63e299fab6239892ceb05318db6c5ffbf02
Andre <andresnu@tutanota.com>
2024-07-11 23:31:22 -0300

    exemplo aula 4

---
+++ Exemplo-aula4/src/main/java/br/com/exemplo/aula/entities/UsuarioEntity.java
@@ -0,0 +1,37 @@
+package br.com.exemplo.aula.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Table(name="usuario")
+@Data
+public class UsuarioEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+}
